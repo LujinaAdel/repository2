05949e65e24bb346ad5fffe1ad4ee61d68123a3a
LujinaAdel <lujinaadel@gmail.com>
2025-08-14 16:45:25 +0300

    oop code

---
+++ mtask.py
@@ -0,0 +1,23 @@
+from  taskoop import person
+
+class TeamLeader(person):
+    def details(self):
+        original_details = super().details()
+        
+        if self.age >= 20:
+            if self.gender == "female":
+                return f"Hello Mrs {self.name} ,{original_details}"
+            else:
+                return f"Hello M. {self.name} ,{original_details}"
+        else:
+            return original_details
+
+
+if __name__ == "main":
+    team_members = []  
+    leader = TeamLeader("Lujina Adel", "female", 20, "Backend Developer", team_members)
+    print(leader.details())
+    leader1 = TeamLeader("Moaz waleed", "Male", 25, "Backend Developer", team_members)
+    print(leader1.details())
+    leader2 = TeamLeader("Hana", "female", 15, "Backend Developer", team_members)
+    print(leader2.details())
---
+++ taskoop.py
@@ -0,0 +1,70 @@
+class person:
+    def __init__(self, name, gender, age, job, position="Employee"):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.job = job
+        self.position = position
+
+    def details(self):
+        return f"your Name: {self.name}\nthe gender: {self.gender}\nthe age: {self.age}\nyour job: {self.job}\nyour position: {self.position}"
+
+class Employees(person):
+    def __init__(self, name, gender, age, job):
+        super().__init__(name, gender, age, job, position="Employee")
+        self.position = "Employee"
+
+    def details(self):
+        return super().details()
+    
+class team_leader(Employees):
+    def __init__(self, name, gender, age, job, team_members):
+        super().__init__(name, gender, age, job)
+        self.team_members = team_members
+        self.position = "Team leader"
+    
+    def details(self):
+        members = [member.name for member in self.team_members]
+        return f"{super().details()}\nTeam Members: {members}"
+    
+
+Ahmed = person("Ahmed Mohamed", "Male", 16, "Backend developer", "Employee")
+
+nour = Employees("Nour Ibrahim", "Male", 16, "Backend Developer")
+hana = Employees("Hana", "Female", 15, "Backend Developer")
+moaz = Employees("Moaz Waleed", "Male", 16, "Backend Developer")
+
+lujina = team_leader("Lujina Adel", "Female", 16, "Backend Developer", [Ahmed, nour, hana, moaz])
+
+print ("hello ! please write your informations:")
+print (input("Name :"))
+print (input("gender :"))
+print (input("age :"))
+print (input("job :"))
+print (input("position :"))
+print("#" * 40)
+print ("hello ! please write your informations:")
+print (input("Name :"))
+print (input("gender :"))
+print (input("age :"))
+print (input("job :"))
+print (input("position : Employee"))
+print("#" * 40)
+print (input("Name :"))
+print (input("gender :"))
+print (input("age :"))
+print (input("job :"))
+print (input("position : Employee"))
+print(input("team Members  (please write in list []): "))
+print("#" * 40)
+
+print("Person Details:")
+print(Ahmed.details())
+
+print("Employees Details:")
+print(nour.details())
+print(hana.details())
+print(moaz.details())
+
+print("Team Leader Details:")
+print(lujina.details())
